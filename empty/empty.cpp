53d445689b8ea26e8dbabbe21bf306f9b5d4e8ac
yumaokao <ymkq9h@gmail.com>
2017-06-14 15:49:11 +0800

    empty: uses clfinish instead of clwaitforevents

--- empty/empty.cpp
+++ empty/empty.cpp
@@ -9,11 +9,11 @@
 #include <cstdlib>
 #include <iostream>
 
-const char * helloStr  = "__kernel void "
-"hello(void) "
-"{ "
-"  "
-"} ";
+const char * helloStr = "__kernel void "
+                        "hello(void) "
+                        "{ "
+                        "  "
+                        "} ";
 
 int main(void)
 {
@@ -25,14 +25,12 @@ int main(void)
             std::cout << "Platform size 0\n";
             return -1;
         }
-        cl_context_properties properties[] = 
-        { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0};
-        cl::Context context(CL_DEVICE_TYPE_CPU, properties); 
+        cl_context_properties properties[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(platforms[0])(), 0};
+        cl::Context context(CL_DEVICE_TYPE_GPU, properties);
 
         std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
-        cl::Program::Sources source(1,
-                std::make_pair(helloStr,strlen(helloStr)));
+        cl::Program::Sources source(1, std::make_pair(helloStr,strlen(helloStr)));
         cl::Program program_ = cl::Program(context, source);
         program_.build(devices);
 
@@ -40,24 +38,13 @@ int main(void)
 
         cl::Event event;
         cl::CommandQueue queue(context, devices[0], 0, &err);
-        queue.enqueueNDRangeKernel(
-                kernel, 
-                cl::NullRange, 
-                cl::NDRange(4,4),
-                cl::NullRange,
-                NULL,
-                &event); 
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(4,4), cl::NullRange, NULL, &event);
 
-        event.wait();
+        queue.finish();
+        // event.wait();
     }
     catch (cl::Error err) {
-        std::cerr 
-            << "ERROR: "
-            << err.what()
-            << "("
-            << err.err()
-            << ")"
-            << std::endl;
+        std::cerr << "ERROR: " << err.what() << "(" << err.err() << ")" << std::endl;
     }
 
     return EXIT_SUCCESS;
